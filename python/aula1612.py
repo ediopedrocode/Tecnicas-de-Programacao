7a2cd4f0b87e3a9d85577664cb850466aaa4ce9c
Édio Pedro <56984327+ediopedrocode@users.noreply.github.com>
2019-12-16 10:27:08 -0300

    Add files via upload

---
+++ python/aula1612.py
@@ -0,0 +1,10 @@
+nome = input("Informe seu nome: ")
+
+idade = int(input("Informe sua idade: "))
+
+if idade<30 :
+    print("Você é jovem.") #ComoComentar
+else:
+    print("És velho.")
+
+print("Oi ", nome, ", tudo bem? Você tem ", idade*2, "anos né?")
---
+++ python/aula1612pt2.py
@@ -0,0 +1,5 @@
+i = 0
+
+while i <= 21 :
+    print(i, end=" ") #escolhendo o fim para não quebrar linha
+    i+=1
